10663d9788427276f5b95d2024f10a001e85d286
BizonNT <137486029+BizonNT@users.noreply.github.com>
2024-01-29 06:52:10 +0200

    Layaut modified

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,8 +3,6 @@ import { Outlet } from 'react-router-dom';
 import Menu from 'components/Menu/Menu';
 import Loader from 'components/Loader/Loader';
 
-import css from './shared-layout.module.css';
-
 const SharedLayout = () => {
   return (
     <>
